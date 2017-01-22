ca42502227f69fce9d6fbb4c836a98c4fdce9890
Tim Kennedy <tim@timkennedy.net>
2017-01-22 00:49:40 -0500

    Adding powershell version for fun.

---
+++ he-ddns-updater/he-ddns-updater.ps1
@@ -0,0 +1,37 @@
+﻿# he-ddns-updater.ps1
+
+$ipsite   = "https://4.ifcfg.me/ip"
+$hostname = '<Dyn DNS Hostname to Update>'
+$authkey  = '<Dyn DNS Auth Key>'
+$ddnsurl  = 'https://dyn.dns.he.net/nic/update'
+$ProgressPreference = "SilentlyContinue"
+$ErrorActionPreference = "SilentlyContinue"
+
+function Write-hr1 {
+    Write-Host ("-" * 80)
+}
+
+function Write-hr2 {
+    Write-Host ("=" * 80)
+}
+
+function Get-WanIP {
+    (Invoke-WebRequest -URI $ipsite).Content -replace "`n|`r"
+}
+
+Write-hr2
+$ip = Get-WanIP
+
+Write-Output "Setting DNS entry for $hostname to point to $ip"
+Write-hr1
+$postParams = @{password=$authkey; hostname=$hostname; myip=$ip}
+$res = Invoke-WebRequest -URI $ddnsurl -Method POST -Body $postParams -DisableKeepAlive -ov ov -ev ev 
+
+if ($res.StatusDescription -eq 'OK') {
+    Write-Host "Success: Dynamic DNS for $hostname updated to $ip" -foreground "green"
+} else {
+    Write-Host "Failure updating Dynamic DNS for $hostname"
+    Write-hr1
+    Write-Host $ev -foreground "red"
+}
+Write-hr2
